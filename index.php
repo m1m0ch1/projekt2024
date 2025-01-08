5e72478556b9e97e41f1645c41cce7dae9f798bb
LMarcell <103493888+m1m0ch1@users.noreply.github.com>
2025-01-08 19:27:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once "api.php";
+require_once "connect.php";
+
+
+// modulárisan
+require_once "header.php";
+
+require_once "main.php";
+
+require_once "footer.php";
+
+?>
+
+
+  
+
+    
